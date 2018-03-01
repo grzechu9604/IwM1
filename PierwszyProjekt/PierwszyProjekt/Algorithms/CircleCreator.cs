1224e7648372bd2ab9b84dd425d0ac6d55fe0380
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-03-01 23:48:08 +0100

    Propozycja generowania okręgu wpisanego w kwadrat + test linii Implementacja generowania okręgu jak najbardziej do poprawy bo nie jest zadowalająca :/

--- PierwszyProjekt/PierwszyProjekt/Algorithms/CircleCreator.cs
+++ PierwszyProjekt/PierwszyProjekt/Algorithms/CircleCreator.cs
@@ -9,11 +9,11 @@ namespace PierwszyProjekt.Algorithms
 {
     public class CircleCreator
     {
-        private long maxX;
-        private long maxY;
+        private int maxX;
+        private int maxY;
         public List<Point> PointsOnCircle { private set; get; }
 
-        public CircleCreator(long maxX, long maxY)
+        public CircleCreator(int maxX, int maxY)
         {
             this.maxX = maxX;
             this.maxY = maxY;
@@ -23,8 +23,52 @@ namespace PierwszyProjekt.Algorithms
         private List<Point> CreateCircle()
         {
             List<Point> points = new List<Point>();
-            
+
+            int radius = maxX / 2;
+
+            //rozwiązanie nieoptymalne
+            for (int x = 0; x < radius; x++)
+            {
+                Tuple<Point, double> min = new Tuple<Point, double>(new Point(x, 0), Math.Abs(Math.Pow(radius - x, 2) + Math.Pow(radius, 2) - Math.Pow(radius, 2)));
+
+                for (int y = 0; y < radius; y++)
+                {
+                    double currentValue = Math.Abs(Math.Pow(radius - x, 2) + Math.Pow(radius - y, 2) - Math.Pow(radius, 2));
+                    if (min.Item2 > currentValue)
+                    {
+                        min = new Tuple<Point, double>(new Point(x, y), currentValue);
+                    }
+                }
+                points.Add(min.Item1);
+            }
+
+            //korzystanie z symetrii przed pętlą są elementy z I ćwiartki 
+            // pomijamy pierwszy i ostatni element zakładając że leżą idealnie po środku
+            List<Point> _points = new List<Point>();
+            _points.AddRange(points);
+            foreach (var point in _points)
+            {
+
+                // IV ćwiartka
+                Point p = new Point(point.X, maxY - point.Y);
+                points.Add(p);
+                // II ćwiartka
+                p = new Point(maxX - point.X, point.Y);
+                points.Add(p);
+                // III ćwiartka
+                p = new Point(maxX - point.X, maxY - point.Y);
+                points.Add(p);
+            }
+
             return points;
         }
+
+        //private Point GenerateValidPoint(int x, int y)
+        //{
+        //    if (x == maxX)
+        //    {
+        //        x = maxX - 1;
+        //    }
+        //}
     }
 }
--- PierwszyProjekt/PierwszyProjekt/Program.cs
+++ PierwszyProjekt/PierwszyProjekt/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PierwszyProjekt.Algorithms;
+using System.Drawing;
 
 namespace PierwszyProjekt
 {
@@ -10,6 +12,30 @@ namespace PierwszyProjekt
     {
         static void Main(string[] args)
         {
+            CircleCreator cc = new CircleCreator(99, 99);
+            int[,] array = new int[100, 100];
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    array[i,j] = 0;
+                }
+            }
+
+            cc.PointsOnCircle.ForEach(p => array[p.X, p.Y] = 1);
+            LineCreator lc = new LineCreator(new Point(6, 6), new Point(54, 99));
+            lc.Line.ForEach(p => array[p.X, p.Y] = 5);
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    Console.Write(array[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
