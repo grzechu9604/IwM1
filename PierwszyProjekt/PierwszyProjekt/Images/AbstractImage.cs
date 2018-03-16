dfe4b2cc700a1eb6daba3f9428f931b81e293274
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-03-16 08:29:36 +0100

    AbstractImage Wydzielenie wspólnych cech BaseImage i Sinogramu do klasy abstrakcyjnej

---
+++ PierwszyProjekt/PierwszyProjekt/Images/AbstractImage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PierwszyProjekt.Images
+{
+    public class AbstractImage
+    {
+        public Bitmap Bitmap { protected set; get; }
+
+        public void Display(PictureBox pictureBox)
+        {
+            pictureBox.Image = Bitmap;
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        protected void NormalizeAverageTab(int maxX, int maxY, double maxAverage, double[,] tabTNormalize)
+        {
+            for (int i = 0; i < maxX; i++)
+            {
+                for (int j = 0; j < maxY; j++)
+                {
+                    tabTNormalize[i, j] = tabTNormalize[i, j] * 255 / maxAverage;
+                }
+            }
+        }
+
+        protected void GenerateBitmap(int maxX, int maxY, double[,] blacAndWhiteTab)
+        {
+            Bitmap = new Bitmap(maxX, maxY);
+
+            for (int i = 0; i < maxX; i++)
+            {
+                for (int j = 0; j < maxY; j++)
+                {
+                    int average = Convert.ToInt32(blacAndWhiteTab[i, j]);
+                    Color c = Color.FromArgb(average, average, average);
+                    Bitmap.SetPixel(i, j, c);
+                }
+            }
+        }
+    }
+}
