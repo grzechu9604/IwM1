3ce781778fe778ea587b46b05226e763ff163ea2
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-03-30 17:06:24 +0200

    Zbędne usingi

--- PierwszyProjekt/PierwszyProjekt/Algorithms/SineEmiterGenerator.cs
+++ PierwszyProjekt/PierwszyProjekt/Algorithms/SineEmiterGenerator.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using PierwszyProjekt.DataTypes;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PierwszyProjekt.DataTypes;
 using System.Drawing;
 
 namespace PierwszyProjekt.Algorithms
